bedf7a2643da58c1753eed8fb6fefeeb5e73b1ef
Aswin M Guptha <aswinmguptha@gmail.com>
2020-07-14 12:15:13 +0530

    Covering some edge cases when someone tries to end attendance before starting it

--- bot.py
+++ bot.py
@@ -82,27 +82,35 @@ Please be noted that the end_attendance command will send the result as csv as a
     def end_attendance(self, update, context):
         original_member = context.bot.get_chat_member(update.effective_chat.id, update.effective_user.id)
         if original_member['status'] in ('creator', 'administrator'):
-            context.bot.edit_message_text(text="Attendance is over. {} people(s) marked attendance.".format(len(context.chat_data['list'])),
+            if 'flag' not in context.chat_data:
+                update.message.reply_text("Please start the attendance first")
+                update.message.delete()
+                return
+            else:
+                '''if 'list' not in context.chat_data:
+                    context.bot.edit_message_text(text="Attendance is over. 0 people marked attendance.",
+                                                  chat_id=self.message.chat_id, message_id=self.message.message_id)
+                else:'''
+                context.bot.edit_message_text(text="Attendance is over. {} people marked attendance.".format(len(context.chat_data['list'])),
                                           chat_id=self.message.chat_id, message_id=self.message.message_id)
-            date_and_time = datetime.now().strftime('%F-%A-%r')
-            filename = f'{update.effective_chat.title}-Attendance-{date_and_time}.csv'
-            caption = f'Attendees: {len(context.chat_data["list"])}\nDate: {datetime.now().strftime("%F")}\nTime: {datetime.now().strftime("%r")}'
-            with StringIO() as f:
-                _writer = csv.writer(f)
-                _writer.writerow(['Serial number','user id','Name','Time'])
-                _writer.writerows(context.chat_data['list'])
-                f.seek(0)
-                f = BytesIO(f.read().encode('utf8'))
-                if(len(context.chat_data['list'])>0):
-                    try:
-                        context.bot.send_document(update.effective_user.id, f, filename=filename, caption=caption)
-                    except Exception as e:
-                        context.bot.send_message(update.effective_chat.id, str(e))
-                        context.bot.send_message(update.effective_chat.id, 'Posting result in the group...')
-                        f.seek(0)
-                        context.bot.send_document(update.effective_chat.id, f, filename=filename, caption=caption)
-
-            del context.chat_data['flag']
+                date_and_time = datetime.now().strftime('%F-%A-%r')
+                filename = f'{update.effective_chat.title}-Attendance-{date_and_time}.csv'
+                caption = f'Attendees: {len(context.chat_data["list"])}\nDate: {datetime.now().strftime("%F")}\nTime: {datetime.now().strftime("%r")}'
+                with StringIO() as f:
+                    _writer = csv.writer(f)
+                    _writer.writerow(['Serial number','user id','Name','Time'])
+                    _writer.writerows(context.chat_data['list'])
+                    f.seek(0)
+                    f = BytesIO(f.read().encode('utf8'))
+                    if len(context.chat_data['list']) > 0:
+                        try:
+                            context.bot.send_document(update.effective_user.id, f, filename=filename, caption=caption)
+                        except Exception as e:
+                            context.bot.send_message(update.effective_chat.id, str(e))
+                            context.bot.send_message(update.effective_chat.id, 'Posting result in the group...')
+                            f.seek(0)
+                            context.bot.send_document(update.effective_chat.id, f, filename=filename, caption=caption)
+                del context.chat_data['flag']
         else:
             update.message.reply_text("Only admins can execute this command")
         update.message.delete()
