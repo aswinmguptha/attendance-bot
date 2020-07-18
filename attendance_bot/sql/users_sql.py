fcfe6c550cfc899d55d9365de48cbdfd8709702d
Shrimadhav U K <gitlab@shrimadhavuk.me>
2020-07-18 08:52:35 +0530

    remove lnguage code from users table

--- attendance_bot/sql/users_sql.py
+++ attendance_bot/sql/users_sql.py
@@ -14,25 +14,22 @@ class Users(BASE):
     chat_id = Column(String, primary_key=True)
     first_name = Column(UnicodeText)
     last_name = Column(UnicodeText)
-    language_code = Column(String)
     chat_type = Column(String)
 
     def __init__(
         self,
-        user_id,
+        chat_id,
         first_name,
         last_name,
-        language_code,
         chat_type
     ):
-        self.user_id = user_id
+        self.chat_id = chat_id
         self.first_name = first_name
         self.last_name = last_name
-        self.language_code = language_code
         self.chat_type = chat_type
 
     def __repr__(self):
-        return "<User {} ({})>".format(self.username, self.user_id)
+        return "<User {} ({})>".format(self.first_name, self.chat_id)
 
 
 Users.__table__.create(checkfirst=True)
@@ -43,7 +40,7 @@ INSERTION_LOCK = threading.RLock()
 def get_chat_by_userid(user_id):
     try:
         return SESSION.query(Users).get(
-            Users.user_id == str(user_id)
+            str(Users.chat_id) == str(user_id)
         )
     finally:
         SESSION.close()
@@ -63,7 +60,6 @@ def add_user(
     user_id,
     first_name,
     last_name,
-    language_code,
     chat_type
 ):
     with INSERTION_LOCK:
@@ -71,27 +67,13 @@ def add_user(
         if adder:
             adder.first_name = first_name
             adder.last_name = last_name
-            adder.language_code = language_code
             adder.chat_type = chat_type
         else:
             adder = Users(
                 user_id,
                 first_name,
                 last_name,
-                language_code,
                 chat_type
             )
-        SESSION.add(adder)
-        SESSION.commit()
-
-
-def update_language(
-    user_id,
-    language_code
-):
-    with INSERTION_LOCK:
-        adder = SESSION.query(Users).get(user_id)
-        if adder:
-            adder.language_code = language_code
         SESSION.add(adder)
         SESSION.commit()
