1f8db13263cb099544cc86dd00904fd212251128
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2021-08-21 13:49:56 +0530

    yyy

--- attendance_bot/modules/on_mark_attendance_btn_press.py
+++ attendance_bot/modules/on_mark_attendance_btn_press.py
@@ -9,6 +9,7 @@ from telegram.ext import CallbackQueryHandler, run_async
 
 from attendance_bot import dispatcher, i18n
 
+from attendance_bot.sql.users_sql import get_chat_by_userid, add_user
 from attendance_bot.sql.attendance_sheet_sql import mark_attendance, check_attendance
 from attendance_bot.helpers.wrappers import into_local_time, localize
 
@@ -17,6 +18,21 @@ from attendance_bot.helpers.wrappers import into_local_time, localize
 @into_local_time
 @localize
 def mark_attendance_fn(update: Update, context, tz=pytz.UTC.zone):
+    user_details = get_chat_by_userid(update.effective_chat.id)
+    if not user_details:
+        chat_type = update.effective_chat.type
+        if chat_type == "private":
+            add_user(
+                update.effective_user.id,
+                update.effective_user.first_name,
+                update.effective_user.last_name or "",
+                chat_type,
+            )
+        else:
+            add_user(
+                update.effective_chat.id, update.effective_chat.title, "", chat_type
+            )
+    #
     tz = pytz.timezone(tz)
     query = update.callback_query
     choice = query.data
